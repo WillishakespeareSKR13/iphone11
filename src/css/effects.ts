3ad789804d03306b7e6794bb96b933742a7ec073
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2022-07-23 23:06:29 -0700

    Feat: shadow effect

--- src/css/effects.ts
+++ src/css/effects.ts
@@ -4,23 +4,31 @@ export const CSSShadowCover = css`
   ::before {
     content: "";
     position: absolute;
-    bottom: -40px;
-    left: -40px;
-    width: 100vw;
+    top: -40px;
+    left: 0;
+    width: 100%;
     height: 150px;
     transform: translate(0, -100%);
     background: linear-gradient(0deg, #000, transparent 100%, transparent);
     z-index: 100;
+    @media screen and (max-width: 768px) {
+      left: -40px;
+      width: 100vw;
+    }
   }
   ::after {
     content: "";
     position: absolute;
     bottom: -40px;
-    left: -40px;
-    width: 100vw;
+    left: 0;
+    width: 100%;
     height: 150px;
     transform: translate(0, 100%);
     background: linear-gradient(180deg, #000, transparent 100%, transparent);
     z-index: 100;
+    @media screen and (max-width: 768px) {
+      left: -40px;
+      width: 100vw;
+    }
   }
 `;
